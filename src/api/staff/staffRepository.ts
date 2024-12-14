361b36ea6f5f88c8c1c7eb7b35ae68cbfd716012
FabioThierry <fabiodomingues.08@gmail.com>
2024-12-14 10:41:15 -0300

    update staffRepositort

--- src/api/staff/staffRepository.ts
+++ src/api/staff/staffRepository.ts
@@ -1,34 +1,34 @@
 import type { Staff } from "@/api/staff/staffModel";
 import db from "@/common/utils/db";
 
-export class OrganizerRepository {
+export class StaffRepository {
   async findAllAsync(): Promise<Staff[]> {
-    const organizers = db.staff.findMany();
-    return organizers;
+    const staffers = db.staff.findMany();
+    return staffers;
   }
 
   async findByIdAsync(id: string): Promise<Staff | null> {
-    const organizer = db.staff.findUnique({ where: { id } });
-    return organizer || null;
+    const staffer = db.staff.findUnique({ where: { id } });
+    return staffer || null;
   }
 
   async createAsync(organizer: Staff): Promise<Staff> {
-    const createdOrganizer = db.staff.create({ data: organizer });
-    return createdOrganizer;
+    const createdStaff = db.staff.create({ data: organizer });
+    return createdStaff;
   }
 
   async updateAsync(organizer: Staff): Promise<Staff> {
-    const updatedOrganizer = db.staff.update({ where: { id: organizer.id }, data: organizer });
-    return updatedOrganizer;
+    const updatedStaff = db.staff.update({ where: { id: organizer.id }, data: organizer });
+    return updatedStaff;
   }
 
   async deleteAsync(id: string): Promise<Staff> {
-    const deletedOrganizer = db.staff.delete({ where: { id } });
-    return deletedOrganizer;
+    const deletedStaff = db.staff.delete({ where: { id } });
+    return deletedStaff;
   }
 
   async findByEmailAsync(email: string): Promise<Staff | null> {
-    const organizer = db.staff.findUnique({ where: { email } });
-    return organizer || null;
+    const staffer = db.staff.findUnique({ where: { email } });
+    return staffer || null;
   }
 }
