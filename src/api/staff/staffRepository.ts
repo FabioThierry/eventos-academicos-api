6a9ce9e0b1cb11f08d7131b680eca37803f91f0c
FabioThierry <fabiodomingues.08@gmail.com>
2024-12-14 10:34:58 -0300

    update staff Repository

--- src/api/staff/staffRepository.ts
+++ src/api/staff/staffRepository.ts
@@ -6,4 +6,29 @@ export class OrganizerRepository {
     const organizers = db.staff.findMany();
     return organizers;
   }
+
+  async findByIdAsync(id: string): Promise<Staff | null> {
+    const organizer = db.staff.findUnique({ where: { id } });
+    return organizer || null;
+  }
+
+  async createAsync(organizer: Staff): Promise<Staff> {
+    const createdOrganizer = db.staff.create({ data: organizer });
+    return createdOrganizer;
+  }
+
+  async updateAsync(organizer: Staff): Promise<Staff> {
+    const updatedOrganizer = db.staff.update({ where: { id: organizer.id }, data: organizer });
+    return updatedOrganizer;
+  }
+
+  async deleteAsync(id: string): Promise<Staff> {
+    const deletedOrganizer = db.staff.delete({ where: { id } });
+    return deletedOrganizer;
+  }
+
+  async findByEmailAsync(email: string): Promise<Staff | null> {
+    const organizer = db.staff.findUnique({ where: { email } });
+    return organizer || null;
+  }
 }
